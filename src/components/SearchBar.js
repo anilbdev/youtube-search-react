7615e9253309b653e560d668d75eed42b59e80fa
Anil Bhasi <anilbhasib@gmail.com>
2021-10-20 20:26:36 +0530

    input made active

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 export class SearchBar extends Component {
+    state={term:''}
+    inputChange=(event)=>{
+        this.setState({term:event.target.value})
+    }
     render() {
         return (
-            <div>
-                SearchBar
+            <div className=" search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text" 
+                            value={this.state.term} 
+                            onChange={this.inputChange}/>
+
+                    </div>
+                </form>
+
             </div>
+          
         )
     }
 }
