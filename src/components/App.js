2ebbce5a875b6e665673c3c0d7a91d154089277f
Anil Bhasi <anilbhasib@gmail.com>
2021-10-21 08:24:16 +0530

    selected video passed to app

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends Component {
-    state={videos:[]}
+    state={videos:[],selectedVideo:null}
 
     onTermSubmit=async (term)=>{
     const response = await youtube.get('/search',{
@@ -16,12 +16,15 @@ class App extends Component {
        this.setState({videos:response.data.items}) 
     }
     
+    onVideoSelect=(video)=>{
+        console.log('From the App :',video);
+    }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoList videos ={this.state.videos}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos ={this.state.videos}/>
             </div>
         )
     }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const VideoItem = ({video}) => {
+import './VideoItem.css'
+
+const VideoItem = ({video,onVideoSelect}) => {
     return (
-        <div className="item">
+        <div onClick={()=>onVideoSelect(video)} className="video-item item">
             <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="" />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
