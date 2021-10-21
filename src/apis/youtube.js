fecf768d0dde2a8185553f87d303ed2d7b2eb278
Anil Bhasi <anilbhasib@gmail.com>
2021-10-21 06:32:52 +0530

    youtube api working

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const KEY = 'AIzaSyAy-nNvonAh4b7fk8KbHy9iThpZ4REW0HI'
 
 export default axios.create({
-    baseURL:'https://www.googleapis.com/youtube/v3',
+    baseURL:'https://www.googleapis.com/youtube/v3/',
     params:{
         part:'snippet',
         maxResults:5,
-        Key:KEY,
+        key:KEY,
         type:'video'
     }
 })
