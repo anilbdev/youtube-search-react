fb42c478203e8a47b734a9697aa66ddb9208cb42
bhasi <anilbhasib@gmail.com>
2021-11-25 10:15:19 +0530

    going for netlify production

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = ({ onFormSubmit }) => {
         <div className=" search-bar ui segment">
             <form onSubmit={onSubmit} className="ui form">
                 <div className="field">
-                    <label>Video Search</label>
+                    <label>Video for a video</label>
                     <input type="text"
                         value={term}
                         onChange={(event) => {
